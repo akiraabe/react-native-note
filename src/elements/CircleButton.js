23c7b216eda6e7608e031ba4706c28e52eb5e95a
akiraabe <akiraabe2005@yahoo.co.jp>
2019-08-27 20:15:14 +0900

    Modify CircleButton style.

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,18 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
     render() {
+        const { style, color } = this.props;
+
+        let bgColor = '#E31676';
+        let textColor = '#fff'
+
+        if (color === 'white') {
+            bgColor = '#fff';
+            textColor = '#E31676';
+        }
         return (
-            <View style={styles.memoAddButton}>
-                <Text style={styles.memoAddButtonTitle}>
+            <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+                <Text style={[styles.circleButtonTitle, { color: textColor }]}>
                     {this.props.children}
                 </Text>
             </View>
@@ -14,21 +23,19 @@ class CircleButton extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    memoAddButton: {
+    circleButton: {
         position: 'absolute',
         bottom: 32,
         right: 32,
         width: 48,
         height: 48,
-        backgroundColor: '#E31676',
         borderRadius: 24,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    memoAddButtonTitle: {
+    circleButtonTitle: {
         fontSize: 32,
         lineHeight: 32,
-        color: '#fff',
     },
 });
 
